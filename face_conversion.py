db23112927534b359642c6de5b85c40eafcc8c58
3-24 <dev.youngseok@gmail.com>
2020-07-24 05:51:44 +0000

    Change parameter

--- face_conversion.py
+++ face_conversion.py
@@ -6,12 +6,11 @@ img = cv2.imread('sample.jpg')
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cascade = cv2.CascadeClassifier(cascadefile)
-facelist = cascade.detectMultiScale(imgray, scaleFactor=2.08, minNeighbors = 1)
+facelist = cascade.detectMultiScale(imgray, scaleFactor=1.2, minNeighbors = 5)
 
-print(facelist)
 if (len(facelist) >= 1):
     for face in facelist:
         x,y,w,h = face
-        cv2.rectangle(img, (x,y), (x+w,y+h), (25,0,0), 2)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
 
 cv2.imwrite('sample_converted.jpg', img)
