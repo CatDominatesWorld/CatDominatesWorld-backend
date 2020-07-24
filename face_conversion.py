c3f937dba4005bf5e2548860346210fec5d9d8a5
3-24 <dev.youngseok@gmail.com>
2020-07-24 06:47:58 +0000

    Update face conversion to use KAKAO API

--- face_conversion.py
+++ face_conversion.py
@@ -1,16 +1,11 @@
-import cv2
+import requests, json
+from config import KAKAO_REST_KEY
 
-cascadefile = "haarcascade_frontalface_default.xml"
+def detect_face(url):
+    API_URL = 'https://kapi.kakao.com/v1/vision/face/detect'
+    headers = {'Authorization': 'KakaoAK {}'.format(KAKAO_REST_KEY)}
+    data = {'image_url': url}
+    response = requests.post(API_URL, data=data, headers=headers)
+    print(response.json())
 
-img = cv2.imread('sample.jpg')
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-cascade = cv2.CascadeClassifier(cascadefile)
-facelist = cascade.detectMultiScale(imgray, scaleFactor=1.2, minNeighbors = 5)
-
-if (len(facelist) >= 1):
-    for face in facelist:
-        x,y,w,h = face
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
-cv2.imwrite('sample_converted.jpg', img)
+detect_face("https://img.hankyung.com/photo/201912/2019122820250418689-540x360.jpg")    
