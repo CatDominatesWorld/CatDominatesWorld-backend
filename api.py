0ae72b942e39e8de5add06707a48497698a5560a
3-24 <dev.youngseok@gmail.com>
2020-07-25 07:57:39 +0000

    Add translate POST endpoint to api.py

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from nyanTranslate import parse_and_convert
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "Welcome to CatDominatesWorld_backend!"
+
+
+@app.route('/translate', methods=['POST'])
+def translateHTML():
+    content = request.json['content']
+    level = request.json('level')
+    response = make_response(parse_and_convert(content,level), 200)
+    response.mimetype = "text/plain"
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
